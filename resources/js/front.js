b285724f16bbc8fe2a3c4f0ff1393b80440eee42
michelafranchini <michela.franchini.91@gmail.com>
2021-08-09 16:16:26 +0200

    Creazione progetto

---
+++ resources/js/front.js
@@ -0,0 +1,16 @@
+window.Vue = require('vue');
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+import App from './App.vue';
+import router from './router'; 
+
+
+const app = new Vue(
+    {
+        el: '#root',
+        render: h => h(App), 
+        router
+    }
+);
---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>BoolAdvisor</title>
+
+        <link rel="stylesheet" href="{{asset('cs/app.css')}}">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
+            
+        </div>
+
+        <script src="{{asset('js/front.js')}}"></script>
+    </body>
+</html>
